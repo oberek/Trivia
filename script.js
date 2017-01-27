248688b996795756781c14fee7db6b35dff42dc9
Maciej <msmusz@student.neumont.edu>
2017-01-27 09:32:13 -0700

    linked css and javascript. added JSON parsing

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 /**
  * Created by Maciej on 1/26/2017.
  */
+var triviaData;
+var request = new XMLHttpRequest();
+
+function loadComplete(evt) {
+    triviaData = JSON.parse(request.responseText);
+    console.log(triviaData);
+    document.getElementById("JSON").innerHTML = triviaData;
+}
+function loadData(){
+    request.open('GET','https://www.opentdb.com/api.php?amount=50');
+    request.onload = loadComplete();
+    request.send();
+}
+
+loadData();
