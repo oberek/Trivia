d2caff9fc52efb635beb0c197aeb6c15e34a1e50
Maciej <msmusz@student.neumont.edu>
2017-01-27 09:53:44 -0700

    fixed the request error

--- script.js
+++ script.js
@@ -3,16 +3,17 @@
  */
 var triviaData;
 var request = new XMLHttpRequest();
+loadData();
 
 function loadComplete(evt) {
+    console.log("IM HERE!");
     triviaData = JSON.parse(request.responseText);
     console.log(triviaData);
     document.getElementById("JSON").innerHTML = triviaData;
 }
 function loadData(){
-    request.open('GET','https://www.opentdb.com/api.php?amount=50');
-    request.onload = loadComplete();
+    request.open('GET','https://opentdb.com/api.php?amount=15');
+    request.onload = loadComplete;
     request.send();
 }
 
-loadData();
